3873340c8fc39dc8b9dafc42c088a9ce96b0cae4
yarik <plehanov.kzn2@gmail.com>
2025-01-08 23:34:07 +0300

    release version

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace coursework
         {
             var menu = new Menu();
             menu.ShowMenu();
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
